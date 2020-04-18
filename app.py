07f7266e8dde366d3fb4aa0779364adf1537dcd6
Viktor Hoffmann <viktor.m.hoffmann@gmail.com>
2020-04-18 11:49:30 +0200

    commenting

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def hello_blank():
     return render_template('page_blank.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')     # Enter local ip
